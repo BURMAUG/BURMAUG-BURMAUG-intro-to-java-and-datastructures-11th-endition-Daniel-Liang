fa8c07cf194fe1da910df875607e39bd560e9615
Burmau G arba <Burmaug@gmail.com>
2021-05-28 14:37:59 -0400

    shuffle exercise

---
+++ src/abstraction13/ShuffleArrayList.java
@@ -0,0 +1,15 @@
+package abstraction13;
+
+import java.util.*;
+
+public class ShuffleArrayList {
+
+    public static void main(String[] args){
+        ArrayList<Number> n = new ArrayList<>(List.of(91,32,12,23,3));
+        shuffleArray(n);
+    }
+    public static void shuffleArray(ArrayList<Number> list){
+        Collections.shuffle(list);
+        System.out.println(list);
+    }
+}
---
+++ src/abstraction13/cloneables/House.java
@@ -0,0 +1,2 @@
+package abstraction13.cloneables;public class House {
+}
