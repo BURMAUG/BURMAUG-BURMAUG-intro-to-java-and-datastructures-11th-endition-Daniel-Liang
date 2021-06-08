86e95607274eb903ed63e3186a1b2870e943289d
Burmau G arba <Burmaug@gmail.com>
2021-06-08 03:34:18 -0400

    started

---
+++ src/abstraction13/interfaces/Apple.java
@@ -0,0 +1,2 @@
+package abstraction13.interfaces;public class Apple {
+}
---
+++ src/banking/Bank.java
@@ -0,0 +1,9 @@
+package banking;
+
+import java.util.ArrayList;
+
+public class Bank {
+    private Customer customer;
+    private ArrayList<Transaction> transactions;
+
+}
